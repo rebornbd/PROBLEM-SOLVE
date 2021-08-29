f173fed6c0c69b7e7e3d4e5f3c4c944cba0eb9e5
Dipta Dhar <dipta.dhar.bd@gmail.com>
2021-08-29 21:43:12 +0600

    35 | searchInsert

--- leetcode/easy/35-SearchInsertPosition.cpp
+++ leetcode/easy/35-SearchInsertPosition.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int searchInsert(vector<int>& nums, int target) {
     int l = 0,
-        r = nums.size() - 1,
-        m = 0;
-
+        r = nums.size() - 1;
+    
     if (nums.size() == 0)
         return 0;
     
-    while (true) {
+    int m;
+    while (r >= l) {
         m = (l+r)/2;
 
         if (nums[m] == target)
@@ -18,14 +18,8 @@ int searchInsert(vector<int>& nums, int target) {
         else if (nums[m] < target)
             l = m + 1;
         else r = m -1;
-
-        if (!(r >= l)) {
-            if (nums[m] < target)
-                return m + 1;
-            return m;
-        }
     }
-
+    return l;
 }
 
 
--- leetcode/easy/35-SearchInsertPosition.py
+++ leetcode/easy/35-SearchInsertPosition.py
@@ -2,12 +2,8 @@
 def searchInsert(nums, target):
     l = 0
     r = len(nums) - 1
-    m = 0
     
-    if len(nums) == 0:
-        return 0
-    
-    while True:
+    while(r >= l):
         m = (l+r) // 2
         
         if nums[m] == target:
@@ -16,15 +12,11 @@ def searchInsert(nums, target):
         elif nums[m] < target:
             l = m + 1
         else: r = m - 1
-        
-        if not r >= l:
-            if nums[m] < target:
-                return m + 1
-            else:
-                return m
+    
+    return l
 
 nums = [1,3,5,6]
-target = 0
+target = 4
 
 res = searchInsert(nums, target)
 print(res)
