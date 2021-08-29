f78e846c92eb861aea3ab8ba543cbfc51f5bc0ae
Dipta Dhar <dipta.dhar.bd@gmail.com>
2021-08-29 15:25:53 +0600

    letcode | 1 - Two Sum

---
+++ leetcode/easy/01-twoSum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    map<int, int> hasNum;
+
+    if (nums.size() > 0)
+        hasNum[nums[0]] = 0;
+    for (int i=1; i<nums.size(); i++) {
+        int nextNumber = target - nums[i];
+
+        if (hasNum.find(nextNumber) != hasNum.end()) {
+            return {hasNum[nextNumber], i};
+        } else {
+            hasNum[nums[i]] = i;
+        }
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> res = twoSum(nums, target);
+
+    if (res.size() > 0) {
+        cout << res[0] << " " << res[1] << endl;
+    }
+    return 0;
+}
